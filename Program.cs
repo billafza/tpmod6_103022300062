4d99f539cb5b58d47dd966b6d225756f6a10d3a9
billafza <nashbillanurfazza@gmail.com>
2025-03-23 20:03:05 +0700

    implementasi design by contract

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿class SayaTubeVideo
+﻿using System.Diagnostics;
+
+class SayaTubeVideo
 {
     int id;
     String title;
@@ -6,6 +8,10 @@
 
     public SayaTubeVideo(String title)
     {
+
+        Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter!");
+        Debug.Assert(title != null, "Judul tidak boleh kosong!");
+
         Random rdmId = new Random();
         this.id = rdmId.Next(00001, 99999);
         this.title = title;
@@ -14,11 +20,22 @@
 
     public void increasePlayCount(int playCount)
     {
-        if (playCount < 0)
+        try
         {
-            Console.WriteLine("Jumlah tidak boleh negatif!");
+            if (playCount < 0 || playCount > 10000000)
+            {
+                throw new ArgumentException("Error : Input Play Count minimal 0 dan maksomal 10.000.000");
+            }
+
+            checked
+            {
+                this.playCount += playCount;
+            }
         }
-        this.playCount += playCount;
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
     }
 
     public void printVideoDetails()
@@ -34,7 +51,9 @@ class program
     static void Main()
     {
         SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Nashbilla Nurfazza");
-        video.increasePlayCount(17);
+        video.increasePlayCount(17000000);
+        video.increasePlayCount(-10);
+        video.increasePlayCount(100000);
         video.printVideoDetails();
     }
 }
--- obj/Debug/net8.0/tpmodul6_103022300062.AssemblyInfo.cs
+++ obj/Debug/net8.0/tpmodul6_103022300062.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul6_103022300062")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edccd5055d9f2507476482dc536996c5e9d077f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4091d9e49256172eb8c74b4cf827d8a47851634")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul6_103022300062")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul6_103022300062")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
